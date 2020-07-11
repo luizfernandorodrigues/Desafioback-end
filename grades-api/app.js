5ef38302a050dc7b6ed1f99c0a53e44292f6a773
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-11 01:18:49 -0300

    ajuste cors

--- grades-api/app.js
+++ grades-api/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://hidden-headland-18817.herokuapp.com',
   })
 );
 
