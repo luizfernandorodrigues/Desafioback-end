c23dc681224408049f78ec43d1f17c486dc58748
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-11 01:19:58 -0300

    ajuste

--- grades-api/app.js
+++ grades-api/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://hidden-headland-18817.herokuapp.com',
+    origin: 'https://hidden-headland-18817.herokuapp.com/',
   })
 );
 
